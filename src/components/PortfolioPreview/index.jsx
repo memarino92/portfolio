98f0776fd79864749b74d14e9bc35629a67bdce7
memarino92 <memarino92@gmail.com>
2022-03-20 00:02:20 -0400

    chore: update astro to 0.24.3

--- src/components/PortfolioPreview/index.jsx
+++ src/components/PortfolioPreview/index.jsx
@@ -4,10 +4,13 @@ import Styles from './styles.module.scss';
 function PortfolioPreview({ project }) {
   return (
     <div className={Styles.card}>
-      <div className={Styles.titleCard} style={`background-image:url(${project.img})`}>
+      <div
+        className={Styles.titleCard}
+        style={`background-image:url(${project.img})`}
+      >
         <h1 className={Styles.title}>{project.title}</h1>
       </div>
-      <div className="pa3">
+      <div className='pa3'>
         <p className={`${Styles.desc} mt0 mb2`}>{project.description}</p>
         <div className={Styles.tags}>
           Tagged:
