daf933053bf9264332dc201f8f558dabe405fa4a
memarino92 <memarino92@gmail.com>
2022-08-14 02:27:01 -0400

    chore: upgraded astro to 1.0.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
+  legacy: {
+    // Example: Add support for legacy Markdown features
+    astroFlavoredMarkdown: true,
+  },
 });
