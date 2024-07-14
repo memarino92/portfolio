e9daf3ab49680918e0662b02ce7245cfb0e161b9
memarino92 <memarino92@gmail.com>
2024-07-14 13:00:30 -0400

    finished first pass of blazor dialog article

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -34,8 +34,7 @@ function Article({ article }) {
 
 export const metadata = {
   title: 'Articles',
-  description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+  description: 'A collection of thoughts on web development',
 }
 
 export default async function ArticlesIndex() {
@@ -43,8 +42,8 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Writing on software design, company building, and the aerospace industry."
-      intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+      title="A collection of thoughts on web development"
+      intro="Here's some more text because the design looks a little empty right now"
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
