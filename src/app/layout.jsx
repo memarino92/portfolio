6f17c0c0cbae2e44bf32964af4edf26a2a8a25cb
memarino92 <memarino92@gmail.com>
2024-07-24 22:27:41 -0400

    added umami script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: {
@@ -19,6 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <Script
+        src="https://umami.michaelmarino.dev/script.js"
+        strategy="lazyOnload"
+        data-website-id="5e50d761-8a49-455a-b979-521b8a0f009f"
+      />
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
